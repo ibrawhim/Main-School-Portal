5ea8ef840b23d4f9663ad2c29dd557c6dfcbb1b1
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-09-04 12:10:39 +0100

    portal

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,25 +1,38 @@
 import React, { useEffect, useState } from 'react'
+import {BsSend} from 'react-icons/Bs'
 
 
 
 const Chat = ({socket}) => {
     const [message, setmessage] = useState("")
+    const [allmessages, setallmessages] = useState([])
     useEffect(() => {
-     
+     if(socket.current){
+       socket.current.on("broadcastMsg",(receivedMessage)=>{
+        //  console.log(receivedMessage);
+         setallmessages([...allmessages,receivedMessage])
+       })
+      }
     }, [])
     
     const sendMessage = () => {
-      console.log(message);
       socket.current.emit("sendMsg",message)
     }
-    socket.current.on("broadcastMsg",(receivedMessage)=>{
-      console.log(receivedMessage);
-    })
   return (
     <>
     <div>
-        <input type="text" onChange={(e)=>setmessage(e.target.value)}/>
-        <button onClick={sendMessage}>SendMessage</button>
+      <div className='flex justify-center items-center'>
+        <input type="text" onChange={(e)=>setmessage(e.target.value)} value={message} className='border border-black p-3'/>
+        <button className='bg-black p-2 text-white my-1' onClick={sendMessage}><BsSend/></button>
+      </div>
+        <div>
+          {allmessages.map((msg,index)=>(
+            <div key={index}>
+              {index}
+              {msg}
+            </div>
+          ))}
+        </div>
     </div>
     </>
   )
--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Help = () => {
   return (
-    <div>Help</div>
+    <>
+      <div className=''>
+        <section className='mx-auto'>
+          <div className=' shadow-xl p-4'>
+            <h4 className='font-semibold'>WHAT DO YOU NEED HELP WITH?</h4>
+              <p className='text-xl text-justify'>This page is solely created to help students answer their questions and help with any issues they have.</p>
+              <textarea name="" id="" cols="30" rows="10" placeholder='Write subject here' className='my-3 border w-full border-black rounded'></textarea>
+              <button className='w-full text-center bg-cyan-800 text-white py-2 rounded'>Send</button>
+          </div>
+        </section>
+      </div>
+    </>
   )
 }
 
